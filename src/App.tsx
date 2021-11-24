b79ca6dd95bd25d50028406777d39d8a59f1ee50
Peterson <petersonsimiao1@gmail.com>
2021-11-24 10:24:12 -0300

    feat(component): criado pagina inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,45 @@
-function App() {
-  return <h1>Hello Word</h1>;
-}
+import { useState } from "react";
+import Card from "./components/Card/Card";
+import FormSearch from "./components/FormSearch/FormSearch";
+import ListTask from "./components/ListTask/ListTask";
+import TaskContext from "./context/tasks";
+import { ITask } from "./shared/types/Task";
+
+const App: React.FC = () => {
+  const [tasks, setTasks] = useState<ITask[]>([]);
+
+  function thisTaskExist({ title }: ITask): boolean {
+    return !!tasks.filter((task) => task.title === title).length;
+  }
+
+  function updateTask(newTask: ITask) {
+    if (thisTaskExist(newTask)) return;
+    setTasks((allTask) => [...allTask, newTask]);
+  }
+
+  function completeTask(newTask: ITask) {
+    if (!thisTaskExist(newTask)) return;
+
+    setTasks((allTask) => {
+      const updatedTask = allTask.map((task) => {
+        if (task.id === newTask.id) task.isComplet = true;
+        return task;
+      });
+
+      return updatedTask;
+    });
+  }
+
+  return (
+    <main>
+      <TaskContext.Provider value={{ tasks, updateTask, completeTask }}>
+        <Card>
+          <FormSearch />
+          <ListTask tasks={tasks} />
+        </Card>
+      </TaskContext.Provider>
+    </main>
+  );
+};
 
 export default App;
